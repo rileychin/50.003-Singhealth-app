da48839653e18a9a5588184fbe85f197486e1f35
prispearly <prispearly@gmail.com>
2021-04-09 10:54:04 +0800

    admin staff UI
    edits to
    admin account
    admin add delete tenant
    staff dashboard

--- lib/pages/adminAddDeleteTenant.dart
+++ lib/pages/adminAddDeleteTenant.dart
@@ -6,28 +6,24 @@ import 'package:singhealth_app/Pages/adminAddTenant.dart';
 import 'package:singhealth_app/Pages/adminDeleteTenant.dart';
 
 class AdminAddDeleteTenant extends StatefulWidget {
-
   final User user;
   final dynamic admin;
   final firestoreInstance = FirebaseFirestore.instance;
 
-  AdminAddDeleteTenant({
-    Key key,
-    this.user,
-    this.admin}) : super(key: key);
+  AdminAddDeleteTenant({Key key, this.user, this.admin}) : super(key: key);
 
   @override
-  _AdminAddDeleteTenantState createState() => _AdminAddDeleteTenantState(user,admin);
+  _AdminAddDeleteTenantState createState() =>
+      _AdminAddDeleteTenantState(user, admin);
 }
 
 class _AdminAddDeleteTenantState extends State<AdminAddDeleteTenant> {
-
   //TODO: add 2 buttons, one for delete one for add
   User user;
   dynamic admin;
   final firestoreInstance = FirebaseFirestore.instance;
 
-  _AdminAddDeleteTenantState(user,admin){
+  _AdminAddDeleteTenantState(user, admin) {
     this.user = user;
     this.admin = admin;
   }
@@ -35,39 +31,52 @@ class _AdminAddDeleteTenantState extends State<AdminAddDeleteTenant> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text('Add or Delete a tenant'),
-      ),
-
-      body:
-        Align(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text('Add or Delete a tenant'),
+        ),
+        body: Align(
           alignment: Alignment.center,
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                onPressed: navigateToAdminAddTenant,
-                child: Text("Add a new tenant"),
-              ),
-              SizedBox(height:10),
-              RaisedButton(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                onPressed: navigateToAdminDeleteTenant,
-                child: Text("Delete an existing tenant"),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: navigateToAdminAddTenant,
+                    child: Text("Add a new tenant"),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: navigateToAdminDeleteTenant,
+                    child: Text("Delete an existing tenant"),
+                  ),
+                ),
+              ],
+            ),
           ),
-        )
-    );
+        ));
   }
 
-
   void navigateToAdminAddTenant() {
-    Navigator.push(context,MaterialPageRoute(builder: (context)=> AdminAddTenant(user:user,admin:admin)));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => AdminAddTenant(user: user, admin: admin)));
   }
 
   void navigateToAdminDeleteTenant() {
-    Navigator.push(context,MaterialPageRoute(builder: (context)=> AdminDeleteTenant(user:user,admin:admin)));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => AdminDeleteTenant(user: user, admin: admin)));
   }
 }
--- lib/pages/staffNoncomplianceDashboard.dart
+++ lib/pages/staffNoncomplianceDashboard.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -8,56 +6,54 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'staffDashboardIncidentDetails.dart';
 
-
 class StaffNonComplianceDashboard extends StatefulWidget {
   @override
-  StaffNonComplianceDashboard({
-    Key key,
-    this.user}) : super(key: key);
+  StaffNonComplianceDashboard({Key key, this.user}) : super(key: key);
 
   final User user;
   final firestoreInstance = FirebaseFirestore.instance;
 
-
-  _StaffNonComplianceDashboardState createState() => _StaffNonComplianceDashboardState(user,firestoreInstance);
+  _StaffNonComplianceDashboardState createState() =>
+      _StaffNonComplianceDashboardState(user, firestoreInstance);
 }
 
-class _StaffNonComplianceDashboardState extends State<StaffNonComplianceDashboard> {
-
+class _StaffNonComplianceDashboardState
+    extends State<StaffNonComplianceDashboard> {
   User user;
   FirebaseFirestore firestoreInstance;
-  List<String> tenantList,incidentList;
-  String institution,shopName,details;
+  List<String> tenantList, incidentList;
+  String institution, shopName, details;
   bool shopSelected = false;
 
-  Uint8List incidentBytes,resolutionBytes;
+  Uint8List incidentBytes, resolutionBytes;
 
-
-  _StaffNonComplianceDashboardState(user,firestoreinstance) {
+  _StaffNonComplianceDashboardState(user, firestoreinstance) {
     this.user = user;
     this.firestoreInstance = firestoreinstance;
   }
 
-  void initState(){
+  void initState() {
     super.initState();
     //future
   }
 
   Future<List<dynamic>> updateNonCompliance() async {
-
-    DocumentReference docRef = firestoreInstance.collection('staff').doc(user.uid);
-    await docRef.get().then<dynamic>((DocumentSnapshot snapshot) => {
-      institution = snapshot.data()['institution']
-    });
-    QuerySnapshot querySnapshot = await firestoreInstance.collection('institution').doc(institution).collection('tenant').get();
+    DocumentReference docRef =
+        firestoreInstance.collection('staff').doc(user.uid);
+    await docRef.get().then<dynamic>((DocumentSnapshot snapshot) =>
+        {institution = snapshot.data()['institution']});
+    QuerySnapshot querySnapshot = await firestoreInstance
+        .collection('institution')
+        .doc(institution)
+        .collection('tenant')
+        .get();
 
     String inc = '';
     querySnapshot.docs.forEach((doc) {
       inc += doc['shopName'] + ':';
-
     });
 
-    inc = inc.substring(0,inc.length-1);
+    inc = inc.substring(0, inc.length - 1);
     tenantList = inc.split(":");
     setState(() {
       tenantList = tenantList;
@@ -66,25 +62,41 @@ class _StaffNonComplianceDashboardState extends State<StaffNonComplianceDashboar
 
   Future<void> getIncidents(int index) async {
     shopName = tenantList[index];
-    QuerySnapshot querySnapshot = await firestoreInstance.collection('institution').doc(institution).collection('tenant').doc(shopName).collection('nonComplianceReport').get();
+    QuerySnapshot querySnapshot = await firestoreInstance
+        .collection('institution')
+        .doc(institution)
+        .collection('tenant')
+        .doc(shopName)
+        .collection('nonComplianceReport')
+        .get();
     String unresolved = '';
     String pending = '';
     String resolved = '';
 
-    querySnapshot.docs.forEach((doc){
-      if (doc['status'] == 'unresolved'){
+    querySnapshot.docs.forEach((doc) {
+      if (doc['status'] == 'unresolved') {
         unresolved += doc['incidentName'] + ':';
-      } else if (doc['status'] == 'pending'){
+      } else if (doc['status'] == 'pending') {
         pending += doc['incidentName'] + ':';
-      } else if (doc['status'] == 'resolved'){
+      } else if (doc['status'] == 'resolved') {
         resolved += doc['incidentName'] + ':';
       }
-
     });
-    if(unresolved.length > 0) {unresolved = unresolved.substring(0,unresolved.length-1);}
-    if(pending.length > 0) {pending = pending.substring(0,pending.length-1);}
-    if(resolved.length > 0){resolved = resolved.substring(0,resolved.length-1);}
-    incidentList = ['Unresolved Incidents'] + unresolved.split(":") + ['Pending Confirmation'] + pending.split(":") + ['Resolved Incidents'] + resolved.split(":");
+    if (unresolved.length > 0) {
+      unresolved = unresolved.substring(0, unresolved.length - 1);
+    }
+    if (pending.length > 0) {
+      pending = pending.substring(0, pending.length - 1);
+    }
+    if (resolved.length > 0) {
+      resolved = resolved.substring(0, resolved.length - 1);
+    }
+    incidentList = ['Unresolved Incidents'] +
+        unresolved.split(":") +
+        ['Pending Confirmation'] +
+        pending.split(":") +
+        ['Resolved Incidents'] +
+        resolved.split(":");
     setState(() {
       incidentList = incidentList;
       shopSelected = true;
@@ -92,104 +104,120 @@ class _StaffNonComplianceDashboardState extends State<StaffNonComplianceDashboar
   }
 
   bool incidentTitle(String listItem) {
-    if (listItem == 'Unresolved Incidents' || listItem == 'Pending Confirmation' || listItem == 'Resolved Incidents') {
+    if (listItem == 'Unresolved Incidents' ||
+        listItem == 'Pending Confirmation' ||
+        listItem == 'Resolved Incidents') {
       return true;
     } else {
       return false;
     }
   }
 
-
   @override
-  Widget build(BuildContext context) =>
-      FutureBuilder(
-          future: updateNonCompliance(),
-          builder: (context,snapshot){
-            if (tenantList == null) {return Center(child: CircularProgressIndicator());}
-            else {
-              if (shopSelected == false) {
-                return Scaffold(
-                  appBar: AppBar(
-                      title: Text("Non Compliance Dashboard")
-                  ),
-                  body: ListView.builder(
-                      padding: const EdgeInsets.all(8),
-                      itemCount: tenantList.length,
-
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                            height: 50,
-                            color: Colors.amber[500],
-                            child: RawMaterialButton(
-                                child: Center(
-                                    child: Text(tenantList[index].toString())),
-                                onPressed: () {getIncidents(index);}
-                            )
-
-                        );
-                      }
-
-
-                  ),
-
-                );
-              } else {
-                return Scaffold(
-                    appBar: AppBar(
-                        title: Text("Non Compliance Report")
-                    ),
-                    body: ListView.builder(
-                      padding: const EdgeInsets.all(8),
-                      itemCount: incidentList.length,
-                      itemBuilder: (BuildContext context, int index){
-                        return Container(
-                            height: 50,
-                            color: incidentTitle(incidentList[index]) ? Colors.amber[500] : Colors.amber[100],
-                            child: RawMaterialButton(
-                              child: Center(
-                                child: Text(incidentList[index]),
-                              ),
-                              onPressed: incidentTitle(incidentList[index]) ? null : (){navigateToIncidentDetails(incidentList[index]);},
-
-                            )
-                        );
-                      },
-
-                    )
-                );
-              }
-            }
-
-
+  Widget build(BuildContext context) => FutureBuilder(
+      future: updateNonCompliance(),
+      builder: (context, snapshot) {
+        if (tenantList == null) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          if (shopSelected == false) {
+            return Scaffold(
+              appBar: AppBar(
+                  title: Text("Non Compliance Dashboard"),
+                  backgroundColor: Colors.amber),
+              body: ListView.builder(
+                  padding: const EdgeInsets.fromLTRB(200.0, 16.0, 200.0, 16.0),
+                  itemCount: tenantList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        height: 50,
+                        color: Colors.amber[200],
+                        child: RawMaterialButton(
+                            child: Center(
+                                child: Text(tenantList[index].toString())),
+                            onPressed: () {
+                              getIncidents(index);
+                            }));
+                  }),
+            );
+          } else {
+            return Scaffold(
+                appBar: AppBar(
+                    title: Text("Non Compliance Report"),
+                    backgroundColor: Colors.amber),
+                body: ListView.builder(
+                  padding: const EdgeInsets.fromLTRB(200.0, 16.0, 200.0, 16.0),
+                  itemCount: incidentList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        height: 50,
+                        color: incidentTitle(incidentList[index])
+                            ? Colors.amber[300]
+                            : Colors.amber[100],
+                        child: RawMaterialButton(
+                          child: Center(
+                            child: Text(incidentList[index]),
+                          ),
+                          onPressed: incidentTitle(incidentList[index])
+                              ? null
+                              : () {
+                                  navigateToIncidentDetails(
+                                      incidentList[index]);
+                                },
+                        ));
+                  },
+                ));
           }
-      );
+        }
+      });
   void navigateToIncidentDetails(String incidentName) async {
-    DocumentReference docRef = firestoreInstance.collection('institution').doc(institution).collection('tenant').doc(shopName).collection('nonComplianceReport').doc(incidentName);
+    DocumentReference docRef = firestoreInstance
+        .collection('institution')
+        .doc(institution)
+        .collection('tenant')
+        .doc(shopName)
+        .collection('nonComplianceReport')
+        .doc(incidentName);
     DocumentSnapshot docSnap = await docRef.get();
-    details = "Location: ${docSnap.data()['location']}\nSummary: ${docSnap.data()['summary']}\nStatus: ${docSnap.data()['status']}";
-    QuerySnapshot querySnapshot = await firestoreInstance.collection('institution').doc(institution).collection('tenant').doc(shopName).collection('nonComplianceReport').doc(incidentName).collection('images').get();
+    details =
+        "Location: ${docSnap.data()['location']}\nSummary: ${docSnap.data()['summary']}\nStatus: ${docSnap.data()['status']}";
+    QuerySnapshot querySnapshot = await firestoreInstance
+        .collection('institution')
+        .doc(institution)
+        .collection('tenant')
+        .doc(shopName)
+        .collection('nonComplianceReport')
+        .doc(incidentName)
+        .collection('images')
+        .get();
     querySnapshot.docs.forEach((element) {
-      if (element.id == "incident_image"){
+      if (element.id == "incident_image") {
         //print(element.data()['data']);
         incidentBytes = Uint8List.fromList(element.data()['data'].cast<int>());
         //print(incidentImage);
       } else if (element.id == "resolution_image") {
         if (element.data()['data'] != null) {
-          resolutionBytes = Uint8List.fromList(element.data()['data'].cast<int>());
+          resolutionBytes =
+              Uint8List.fromList(element.data()['data'].cast<int>());
         }
-
-      }});
+      }
+    });
     print(docSnap.id);
-    await Navigator.push(context, MaterialPageRoute(builder:(context) => StaffDashboardIncidentDetails(user: user, details: details, incidentName: incidentName, incidentBytes: incidentBytes, resolutionBytes: resolutionBytes, firestoreInstance: firestoreInstance,
-        docRef: docRef)));
+    await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => StaffDashboardIncidentDetails(
+                user: user,
+                details: details,
+                incidentName: incidentName,
+                incidentBytes: incidentBytes,
+                resolutionBytes: resolutionBytes,
+                firestoreInstance: firestoreInstance,
+                docRef: docRef)));
     setState(() {
       shopSelected = false;
     });
     incidentBytes = null;
     resolutionBytes = null;
-
-
   }
-
-
 }
